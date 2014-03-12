01904f3128718698157ea8d33de0914fc1d0282b
Jonathan Selander <jonathan@mondaycreative.se>
2014-03-12 12:13:23 +0100

    more correctly fetch the order in the payment fee totals

--- src/app/code/community/Svea/WebPay/Model/Sales/Order/Pdf/Total/Paymentfee.php
+++ src/app/code/community/Svea/WebPay/Model/Sales/Order/Pdf/Total/Paymentfee.php
@@ -10,14 +10,20 @@ class Svea_WebPay_Model_Sales_Order_Pdf_Total_Paymentfee
      */
     public function getAmount()
     {
-        $payment = $this->getSource()->getOrder()->getPayment();
+        $source = $this->getSource();
+        if (!($source instanceof Mage_Sales_Model_Order)) {
+            $order = $source->getOrder();
+        } else {
+            $order = $source;
+        }
+        $payment = $order->getPayment();
         $paymentFee = $payment->getAdditionalInformation('svea_payment_fee');
         $method = $payment->getMethodInstance()->getCode();
 
         if (empty($paymentFee) || $method != 'svea_invoice') {
             return;
         }
-        
+
         return $paymentFee;
     }
 }
