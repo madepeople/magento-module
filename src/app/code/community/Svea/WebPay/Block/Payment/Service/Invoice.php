c6bf7863add3ea9ef27e25f235742938d10ca262
Jonathan Selander <selander.jonathan@gmail.com>
2014-06-01 23:05:22 +0200

    hide the invoice phtml in a quick checkout if no invoice info is entered

--- src/app/code/community/Svea/WebPay/Block/Payment/Service/Invoice.php
+++ src/app/code/community/Svea/WebPay/Block/Payment/Service/Invoice.php
@@ -3,12 +3,19 @@
 /**
  * Invoice information block used in the checkout process
  */
-class Svea_WebPay_Block_Payment_Service_Invoice extends Svea_WebPay_Block_Payment_Service_Abstract
+class Svea_WebPay_Block_Payment_Service_Invoice
+    extends Svea_WebPay_Block_Payment_Service_Abstract
 {
-
-    protected $_template = 'svea/payment/service/invoice.phtml';
     protected $_logoCode = 'svea_invoice';
 
-
-
+    protected function _construct()
+    {
+        $invoiceInfo = trim(Mage::getStoreConfig('payment/svea_invoice/invoice_info'));
+        if (!empty($invoiceInfo)) {
+            if (Mage::helper('svea_webpay')->usingQuickCheckout()) {
+                $this->setData('template', 'svea/payment/service/invoice.phtml');
+            }
+        }
+        return parent::_construct();
+    }
 }
