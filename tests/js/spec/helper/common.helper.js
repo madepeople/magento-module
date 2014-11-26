aa2d567d8be6c90264cce48200e23bf614b1320b
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2014-11-26 11:50:34 +0100

    Added missing ;

--- tests/js/spec/helper/common.helper.js
+++ tests/js/spec/helper/common.helper.js
@@ -6,24 +6,24 @@
 /** Get billing country element */
 var getBillingCountryElement = function() {
     return jQuery('#billing\\:country_id');
-}
+};
 
 /** Get billing country */
 var getBillingCountry = function() {
     return getBillingCountryElement().val();
-}
+};
 
 /** Set billing country */
 var setBillingCountry = function(countryCode) {
     getBillingCountryElement().val(countryCode).trigger('change');
-}
+};
 
 /** Get current selected payment method */
 var getPaymentMethod = function() {
     return jQuery('input[name="payment[method]"]:checked').val();
-}
+};
 
 /** Set current selected payment method */
 var setPaymentMethod = function(name) {
     jQuery('#p_method_' + name).trigger('click');
-}
+};
