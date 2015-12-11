7d8992c4fdb6bb6bda800737a39b5cc73d777d9b
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2015-12-11 10:21:35 +0100

    SVEA-116: Improved error message

--- src/app/code/community/Svea/WebPay/Model/System/Config/Backend/Customer/Import.php
+++ src/app/code/community/Svea/WebPay/Model/System/Config/Backend/Customer/Import.php
@@ -30,46 +30,51 @@ class Svea_Webpay_Model_System_Config_Backend_Customer_Import extends Mage_Core_
         if (empty(@$_FILES['groups']['tmp_name']['svea_general']['fields']['import_customer_addresses']['value'])) {
             return $this;
         } else {
-            $csvFile = $_FILES['groups']['tmp_name']['svea_general']['fields']['import_customer_addresses']['value'];
-            $io     = new Varien_Io_File();
-            $info   = pathinfo($csvFile);
-            $io->open(array('path' => $info['dirname']));
-            $io->streamOpen($info['basename'], 'r');
+            try {
+                $csvFile = $_FILES['groups']['tmp_name']['svea_general']['fields']['import_customer_addresses']['value'];
+                $io     = new Varien_Io_File();
+                $info   = pathinfo($csvFile);
+                $io->open(array('path' => $info['dirname']));
+                $io->streamOpen($info['basename'], 'r');
 
-            // delete all current rows
-            $resource = Mage::getSingleton('core/resource');
-            $connection = $resource->getConnection('core_write');
-            $connection->query("DELETE FROM {$resource->getTableName('svea_webpay/customer_address')}");
+                // delete all current rows
+                $resource = Mage::getSingleton('core/resource');
+                $connection = $resource->getConnection('core_write');
+                $connection->query("DELETE FROM {$resource->getTableName('svea_webpay/customer_address')}");
 
-            // read header
-            $header = array();
-            foreach ($io->streamReadCsv(';') as $k => $v) {
-                $header[$k] = $this->_fileHeaderMapping[$this->_convertCsvString($v)];
+                // read header
+                $header = array();
+                foreach ($io->streamReadCsv(';') as $k => $v) {
+                    $header[$k] = $this->_fileHeaderMapping[$this->_convertCsvString($v)];
+                }
+
+                // Import rows
+                while ($row = $io->streamReadCsv(';')) {
+                    $row = array_combine($header, $row);
+                    foreach ($row as $k => $v) {
+                        $row[$k] = $this->_convertCsvString($v);
+                    }
+                    if ($row['status'] !== 'Active') {
+                        continue;
+                    }
+
+                    // Split name into first and last name
+                    foreach ($this->_splitName($row['name']) as $k => $v) {
+                        $row[$k] = $v;
+                    }
+                    unset($row['name']);
+
+                    $model = Mage::getModel('svea_webpay/customer_address');
+                    $model->setOrgnr($row['nationalIdNumber']);
+                    $model->setCountryCode($row['countryCode']);
+                    $model->setAddress($row);
+                    $model->save();
+                }
+                $io->streamClose();
+            } catch (Exception $e) {
+                Mage::logException($e);
+                throw new Mage_Exception(Mage::helper('svea_webpay')->__("Failed to import customer addresses."));
             }
-
-            // Import rows
-            while ($row = $io->streamReadCsv(';')) {
-                $row = array_combine($header, $row);
-                foreach ($row as $k => $v) {
-                    $row[$k] = $this->_convertCsvString($v);
-                }
-                if ($row['status'] !== 'Active') {
-                    continue;
-                }
-
-                // Split name into first and last name
-                foreach ($this->_splitName($row['name']) as $k => $v) {
-                    $row[$k] = $v;
-                }
-                unset($row['name']);
-
-                $model = Mage::getModel('svea_webpay/customer_address');
-                $model->setOrgnr($row['nationalIdNumber']);
-                $model->setCountryCode($row['countryCode']);
-                $model->setAddress($row);
-                $model->save();
-            }
-            $io->streamClose();
         }
     }
 
